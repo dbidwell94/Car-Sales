cb6a2f7d9f7f52cfa5591b4d8eced95825f74a72
Devin Bidwell <dbidwell@intuituscyber.com>
2020-08-11 12:29:52 -0700

    wip

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,23 @@
+import { iFeature } from "../reducers";
+
+export type iType = "ADD_NEW_FEATURE_TO_CAR" | "REMOVE_FEATURE_FROM_CAR";
+
 export type iAction = {
-    type: string;
-    payload: any
+  type: iType;
+  payload: any;
+};
+
+function dispatchAction(type: iType, payload: any): iAction {
+  return {
+    type: type,
+    payload: payload,
+  };
+}
+
+export function addNewFeatureToCar(featureToAdd: iFeature): iAction {
+  return dispatchAction("ADD_NEW_FEATURE_TO_CAR", featureToAdd);
+}
+
+export function removeFeatureFromCar(featureToRemove: iFeature): iAction {
+  return dispatchAction("REMOVE_FEATURE_FROM_CAR", featureToRemove);
 }
